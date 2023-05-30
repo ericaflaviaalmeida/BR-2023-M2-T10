7fbaceadc6133766f8f94893e2b302d8b381e2a6
ericaflaviaalmeida <ericafalmeid@gmail.com>
2023-05-29 22:21:41 -0300

    class dinosaur( )

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,9 +1,10 @@
 import pygame
 
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
-
+from dino_runner.components.dinosaur import Dinosaur
 
 class Game:
+    #aqui altera background
     def __init__(self):
         pygame.init()
         pygame.display.set_caption(TITLE)
@@ -14,6 +15,8 @@ class Game:
         self.game_speed = 20
         self.x_pos_bg = 0
         self.y_pos_bg = 380
+
+        self.player = Dinosaur()
 
     def run(self):
         # Game loop: events - update - draw
@@ -30,14 +33,17 @@ class Game:
                 self.playing = False
 
     def update(self):
-        pass
+        user_input = pygame.key.get_pressed()
+        self.player.update(user_input)
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
+        self.player.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
+
 
     def draw_background(self):
         image_width = BG.get_width()
@@ -46,4 +52,4 @@ class Game:
         if self.x_pos_bg <= -image_width:
             self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
             self.x_pos_bg = 0
-        self.x_pos_bg -= self.game_speed
+        self.x_pos_bg -= self.game_speed #da o movimento de um background seguindo do outro
