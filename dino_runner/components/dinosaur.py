9a310d58d8e9888db42a3a73c502b417fbbc7c92
ericaflaviaalmeida <ericafalmeid@gmail.com>
2023-05-30 17:48:36 -0300

    y_pos_duck atualização

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -4,7 +4,7 @@ from dino_runner.utils.constants import RUNNING,JUMPING,DUCKING
 X_POS = 80
 Y_POS = 310
 JUMP_VEL = 8.5
-Y_POS_DUCK = 400
+Y_POS_DUCK = 350
 
 class Dinosaur:
     def __init__(self):
@@ -18,6 +18,7 @@ class Dinosaur:
         self.dino_jump = False
         self.dino_duck = False
         
+        
     def update(self, user_input):
         if self.dino_run:
             self.run()
@@ -30,10 +31,7 @@ class Dinosaur:
             self.dino_jump = True
             self.dino_run = False
             self.dino_duck = False
-        elif not self.dino_jump:
-            self.dino_jump = False
-            self.dino_run = True
-            self.dino_duck = False
+       
         elif user_input[pygame.K_DOWN] and not self.dino_jump:
             self.dino_run = False
             self.dino_jump = False
